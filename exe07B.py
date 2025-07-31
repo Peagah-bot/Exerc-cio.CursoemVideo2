b44296ac5014ff7c3c739ddecb3ad0731d79cd47
Pedro Vasconcelos <pedrohenriquedeoliveiravasconc@gmail.com>
2025-07-31 14:46:28 -0300

    Add files via upload

---
+++ exe07B.py
@@ -0,0 +1,6 @@
+nome = input('Olá querido professor, para darmos andamento, nos diga, qual o seu nome?')
+print('Muito prazer {}.'.format(nome))
+a1 = float(input('Nos diga, qual a 1° nota que deseja colocar?'))
+a2 = float(input('Certo, agora qual a segunda nota?'))
+print('Certo, a nota de {} e {} em conjunto, tem uma média de: {}'.format(a1, a2, (a1 + a2)/2))
+
